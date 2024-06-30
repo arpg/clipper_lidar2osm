006ce2657068b8a66570ca6edb91020e1a10bf41
donceykong <donceyalbin@gmail.com>
2024-06-30 17:30:37 -0600

    Updated example to show semantics on target cloud

--- examples/python/kitti360_osm_example/osm_example.py
+++ examples/python/kitti360_osm_example/osm_example.py
@@ -80,15 +80,16 @@ def generate_dataset(pcd, pcd2, n1, n2, nia, noa, sigma, T_21):
     Agood = np.tile(np.arange(n1).reshape(-1, 1), (1, 2))
 
     # Incorrect association to draw from
-    Abad = np.zeros((n1 * n2 - n1, 2))
-    itr = 0
-    for i in range(n1):
-        for j in range(n2):
-            if i == j:
-                continue
-            Abad[itr, :] = [i, j]
-            itr += 1
+    # Abad = np.zeros((n1 * n2 - n1, 2))
+    # itr = 0
+    # for i in range(n1):
+    #     for j in range(n2):
+    #         if i == j:
+    #             continue
+    #         Abad[itr, :] = [i, j]
+    #         itr += 1
 
+    Abad = generate_Abad(n1, n2)
     # Sample good and bad associations to satisfy total
     # num of associations with the requested outlier ratio
     IAgood = np.random.choice(Agood.shape[0], nia, replace=False)
@@ -99,50 +100,6 @@ def generate_dataset(pcd, pcd2, n1, n2, nia, noa, sigma, T_21):
     Agt = Agood[IAgood, :]
 
     return (D1, D2, Agt, A)
-
-
-# def generate_dataset(pcd_1, pcd_2, n1, n2, nia, noa, T_21):
-#     """Generate Dataset"""
-#     if nia > n1:
-#         raise ValueError(
-#             "Cannot have more inlier associations "
-#             "than there are model points. Increase"
-#             "the number of points to sample from the"
-#             "original point cloud model."
-#         )
-
-#     # Get points from pcds
-#     D1 = np.asarray(pcd_1.points).T
-#     D2 = np.asarray(pcd_2.points).T
-
-#     # Rotate into view 2 using ground truth transformation
-#     D2_tf = T_21[0:3, 0:3] @ D2 + T_21[0:3, 3].reshape(-1, 1)
-
-#     # Correct associations to draw from
-#     Agood = np.tile(np.arange(nia).reshape(-1, 1), (1, 2))
-
-#     # Incorrect association to draw from
-#     Abad = generate_Abad(n1, n2)
-#     # print("STARTING ABAD")
-#     # Abad = np.zeros((n1 * n2 - nia, 2))
-#     # itr = 0
-#     # for i in range(n1):
-#     #     for j in range(n2):
-#     #         if i == j:
-#     #             continue
-#     #         Abad[itr, :] = [i, j]
-#     #         itr += 1
-#     # print("Finished ABAD")
-
-#     # Sample good and bad associations to satisfy total
-#     # num of associations with the requested outlier ratio
-#     IAgood = np.random.choice(Agood.shape[0], nia, replace=False)
-#     IAbad = np.random.choice(Abad.shape[0], noa, replace=False)
-#     A = np.concatenate((Agood[IAgood, :], Abad[IAbad, :])).astype(np.int32)
-#     # Ground truth associations
-#     Agt = Agood[IAgood, :]
-
-#     return (D1, D2_tf, Agt, A)
 
 
 def get_err(T, That):
@@ -208,55 +165,55 @@ data_dir = f"../../data/kitti360_osm_data/2013_05_28_drive_{seq:04d}_sync/"
 osm_frames = get_frame_numbers(data_dir)
 print(f"len(osm_frames): {len(osm_frames)}")
 
-pointcloud_1_building, pointcloud_1_road = get_accum_points(
-    first_frame_idx=0,
-    final_frame_idx=50,
-    data_dir=data_dir,
-    osm_frames=osm_frames,
-)
-pointcloud_1_building_xyz = pointcloud_1_building[:, :3]
-pointcloud_1_road_xyz = pointcloud_1_road[:, :3]
-pointcloud_1_building_semantic = pointcloud_1_building[:, 4]
-pointcloud_1_road_semantic = pointcloud_1_road[:, 4]
-pointcloud_1_xyz_combined = np.concatenate(
-    (pointcloud_1_building_xyz, pointcloud_1_road_xyz), axis=0
-)
-target_pointcloud_semantic_combined = np.concatenate(
-    (pointcloud_1_building_semantic, pointcloud_1_road_semantic), axis=0
-)
-target_pointcloud_rgb = labels2RGB2(target_pointcloud_semantic_combined, labels_dict)
-target_pcd = o3d.geometry.PointCloud()
-target_pcd.points = o3d.utility.Vector3dVector(pointcloud_1_xyz_combined)
-target_pcd.colors = o3d.utility.Vector3dVector(target_pointcloud_rgb)
-
-pointcloud_2_building, pointcloud_2_road = get_accum_points(
+target_pointcloud_building, target_pointcloud_road = get_accum_points(
     first_frame_idx=0,
     final_frame_idx=len(osm_frames) - 1,
     data_dir=data_dir,
     osm_frames=osm_frames,
 )
-pointcloud_2_building_xyz = pointcloud_2_building[:, :3]
-pointcloud_2_road_xyz = pointcloud_2_road[:, :3]
-pointcloud_2_combined = np.concatenate(
-    (pointcloud_2_building_xyz, pointcloud_2_road_xyz), axis=0
+target_pointcloud_building_xyz = target_pointcloud_building[:, :3]
+target_pointcloud_road_xyz = target_pointcloud_road[:, :3]
+target_pointcloud_building_semantic = target_pointcloud_building[:, 4]
+target_pointcloud_road_semantic = target_pointcloud_road[:, 4]
+target_pointcloud_xyz_combined = np.concatenate(
+    (target_pointcloud_building_xyz, target_pointcloud_road_xyz), axis=0
 )
-pcd_2 = o3d.geometry.PointCloud()
-pcd_2.points = o3d.utility.Vector3dVector(pointcloud_2_combined)
-pcd_2.paint_uniform_color(np.array([0, 1, 0]))
+target_pointcloud_semantic_combined = np.concatenate(
+    (target_pointcloud_building_semantic, target_pointcloud_road_semantic), axis=0
+)
+target_pointcloud_rgb = labels2RGB2(target_pointcloud_semantic_combined, labels_dict)
+target_pcd = o3d.geometry.PointCloud()
+target_pcd.points = o3d.utility.Vector3dVector(target_pointcloud_xyz_combined)
+target_pcd.colors = o3d.utility.Vector3dVector(target_pointcloud_rgb)
 
-o3d.visualization.draw_geometries([target_pcd, pcd_2])
+source_pointcloud_building, source_pointcloud_road = get_accum_points(
+    first_frame_idx=0,
+    final_frame_idx=150,
+    data_dir=data_dir,
+    osm_frames=osm_frames,
+)
+source_pointcloud_building_xyz = source_pointcloud_building[:, :3]
+source_pointcloud_road_xyz = source_pointcloud_road[:, :3]
+source_pointcloud_combined = np.concatenate(
+    (source_pointcloud_building_xyz, source_pointcloud_road_xyz), axis=0
+)
+source_pcd = o3d.geometry.PointCloud()
+source_pcd.points = o3d.utility.Vector3dVector(source_pointcloud_combined)
+source_pcd.paint_uniform_color(np.array([0, 1, 0]))
+
+o3d.visualization.draw_geometries([target_pcd, source_pcd])
 
 # Generate dataset
 percent_input_points = 0.01
-# number of points used on model (i.e., seen in view 1)
+# number of points used on model (This is the number of points in target?)
 n1 = round(percent_input_points * len(target_pcd.points))
-# number of outliers in data (i.e., seen in view 2)
-n2o = round(percent_input_points * len(pcd_2.points))
+# number of outliers in data (points from both target and source that do not overlap)
+n2o = round(percent_input_points * len(source_pcd.points))
 n2 = n1 + n2o  # number of points in view 2
 sigma = 0.01  # uniform noise [m] range
 
-m = 2000  # total number of associations in problem
-outrat = 0.9  # outlier ratio of initial association set
+m = n1  # total number of associations in problem
+outrat = 0.5  # outlier ratio of initial association set
 noa = round(m * outrat)  # number of outlier associations
 nia = m - noa  # number of inlier associations
 
@@ -265,7 +222,7 @@ T_21 = np.eye(4)
 T_21[0:3, 0:3] = Rotation.random().as_matrix()
 T_21[0:3, 3] = np.random.uniform(low=-5, high=5, size=(3,))
 
-D1, D2, Agt, A = generate_dataset(pcd_2, target_pcd, n1, n2, nia, noa, sigma, T_21)
+D1, D2, Agt, A = generate_dataset(target_pcd, source_pcd, n1, n2, nia, noa, sigma, T_21)
 
 # View unaligned data
 pcd_2_tf = o3d.geometry.PointCloud()
@@ -292,7 +249,7 @@ t0 = time.perf_counter()
 clipper.solve()
 t1 = time.perf_counter()
 
-# A = clipper.get_initial_associations()
+A = clipper.get_initial_associations()
 Ain = clipper.get_selected_associations()
 
 p = np.isin(Ain, Agt)[:, 0].sum() / Ain.shape[0]
@@ -305,15 +262,16 @@ print(
 # 1st point cloud (road)
 model = o3d.geometry.PointCloud()
 model.points = o3d.utility.Vector3dVector(D1.T)
-model.paint_uniform_color(np.array([0, 1, 0]))
+model.colors = o3d.utility.Vector3dVector(target_pointcloud_rgb)
 
 
 data = o3d.geometry.PointCloud()
 data.points = o3d.utility.Vector3dVector(D2.T)
-data.paint_uniform_color(np.array([1, 0, 0]))
+data.paint_uniform_color(np.array([0, 1, 0]))
 
-# corr = o3d.geometry.LineSet.create_from_point_cloud_correspondences(model, data, Ain)
-# o3d.visualization.draw_geometries([model, data, corr])
+# Draw pc correspondances
+corr = o3d.geometry.LineSet.create_from_point_cloud_correspondences(model, data, Ain)
+o3d.visualization.draw_geometries([model, data, corr])
 
 p2p = o3d.pipelines.registration.TransformationEstimationPointToPoint()
 That_21 = p2p.compute_transformation(model, data, o3d.utility.Vector2iVector(Ain))
